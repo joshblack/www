7a0a3f2d8604bac5bf959f9e6e483f4181185283
Josh Black <joshblack@github.com>
2024-03-25 17:32:42 -0500

    fix(MDX): set width on columns for overflow in code snippets

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="px-[--page-padding-inline] py-4 text-sm text-neutral-600">
+    <footer className="mt-12 px-[--page-padding-inline] py-4 text-sm text-neutral-600">
       <span>Josh Black © {new Date().getFullYear()}</span>
     </footer>
   );
