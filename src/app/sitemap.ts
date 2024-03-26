e503a311649c4db35a04e8442ffc70bedf8d334f
Josh Black <joshblack@github.com>
2024-03-25 20:03:22 -0500

    feat: add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,22 @@
+import type { MetadataRoute } from 'next';
+import { generateStaticParams } from './writing/[...slug]/page';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await generateStaticParams();
+  return [
+    {
+      url: 'https://josh.black',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://josh.black/writing',
+      lastModified: new Date(),
+    },
+    ...posts.map(({ slug }) => {
+      return {
+        url: `https://josh.black/writing/${slug.join('/')}`,
+        lastModified: new Date(),
+      };
+    }),
+  ];
+}
