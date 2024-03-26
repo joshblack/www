d85a2dec7bd86567f5c5de8306e4bc98f4935796
Josh Black <joshblack@github.com>
2024-03-25 19:40:08 -0500

    feat: enable speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 import { BaseLayout } from '../components/BaseLayout';
 
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <BaseLayout>{children}</BaseLayout>
+        <SpeedInsights />
       </body>
     </html>
   );
